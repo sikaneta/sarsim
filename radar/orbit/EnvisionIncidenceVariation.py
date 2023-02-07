Fixed JPL spreadsheet CBE estimate covariance matrix. Was using 2-sigma value instead of 1-sigma. Also removed all platform contributions to this CBE

radar/orbit/EnvisionIncidenceVariation.py
@@ -16,7 +16,7 @@ from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
 
-#%% Load state vectors from envision
+#%% Load state vectors for envision
 sv = loadSV(toPCR = False)
 
 #%% Get the period
@@ -46,14 +46,40 @@ while idx < N:
 #%% Check the time differences
 z = [sv.measurementData[k][2] for k in idxarr]
 plt.figure()
-plt.plot(z)
+plt.plot(tascarr[0:-1], z)
+plt.grid()
+plt.show()
+
+period = [2*np.pi*np.sqrt(venSAR.state2kepler(sv.measurementData[k])['a']**3
+                          /venSAR.planet.GM) for k in idxarr]
+plt.figure()
+plt.plot(tascarr[0:-1], period)
+plt.xlabel('Time')
+plt.ylabel('Orbit Period (s)')
+plt.title('Orbit period for ET1 2031 N')
+plt.grid()
+plt.show()
+
+
+a = [venSAR.state2kepler(sv.measurementData[k])['a']/1e3 for k in idxarr]
+plt.figure()
+plt.plot(tascarr[0:-1], a)
+plt.xlabel('Time')
+plt.ylabel('Orbit semi-major axis (km)')
+plt.title('Orbit semi-major axis for ET1 2031 N')
 plt.grid()
 plt.show()
 
 plt.figure()
-plt.plot(np.diff(tascarr)/np.timedelta64(1,'m'))
+plt.plot(tascarr[0:-1], np.diff(tascarr)/np.timedelta64(1,'m'))
 plt.grid()
+plt.title('Orbit period (minutes)')
+plt.xlabel('Time')
+plt.ylabel('Orbit period (minutes)')
 plt.show()
+
+cycle_jump = np.argwhere(np.diff(np.diff(tascarr)/np.timedelta64(1,'m')) > 0.02)[:,0] + 1
+
 
 """ If everything checks out, then we have an array of 
     times of the ascending node. This will help to define
@@ -68,11 +94,12 @@ cycleDuration = np.timedelta64(350412, 'm')
 
 #%% Generate some data The orbit number is the actually the state vector number
 svindeces = [29803]
-svindeces = [9803]
-off_nadir = np.arange(20,40,1)
+svindeces = [9903]
+off_nadir = np.arange(00,40,0.01)
 incidence = np.zeros((len(off_nadir), len(svindeces)), dtype = float)
 ranges = np.zeros((len(off_nadir), len(svindeces)), dtype = float)
 rhats = np.zeros((len(off_nadir), 3, len(svindeces)), dtype = float)
+ground_range = np.zeros((len(off_nadir), len(svindeces)), dtype = float)
 
 #%%
 for k, idx in enumerate(svindeces):
@@ -80,7 +107,7 @@ for k, idx in enumerate(svindeces):
      rhats[:,:,k], 
      incidence[:,k], 
      _, 
-     _) = getTiming(sv, 
+     ground_range[:,k]) = getTiming(sv, 
                     np.radians(off_nadir), 
                     idx = idx)
 
@@ -92,17 +119,51 @@ plt.ylabel("Incidence angle (deg)")
 plt.grid()
 plt.show()
 
+targetIncidenceAngle = 30
+groundSwath = 57e3
+targetIDX = np.argmin(np.abs(incidence-targetIncidenceAngle))
+idx = np.argwhere(np.abs(ground_range - 
+                         ground_range[targetIDX]) 
+                  < groundSwath/2)[:,0]
+
 #%% Compute the coordinates of the ground point and surface normal
-xG = sv.measurementData[svindeces[0]][0:3] + ranges[8,0]*rhats[8,:,0]
+xG = sv.measurementData[svindeces[0]][0:3] + ranges[targetIDX,0]*rhats[targetIDX,:,0]
 xG_snormal = surfaceNormal(xG,sv)
+groundPoint = tuple(sv.xyz2polar(xG))
+
+
+#%% Compute the satellite position on the next orbit
+targetSV = sv.measurementData[svindeces[0]]
+venPeriod = np.timedelta64(int(venSAR.period), 's')
+targetSVtime = sv.measurementTime[svindeces[0]] + 5*venPeriod
+R, inc, satSV = computeImagingGeometry(sv, targetSVtime, xG, xG_snormal)
+sposDiff = np.linalg.norm(targetSV[:3] - satSV[1][:3])
+print(np.linalg.norm(sposDiff))
+
+#%%
+plt.figure()
+plt.plot(ground_range/1e3, incidence)
+plt.plot(ground_range[idx]/1e3, incidence[idx], 'g.')
+plt.plot(ground_range[targetIDX]/1e3, incidence[targetIDX], 'ro')
+grdiff = np.linalg.norm(sposDiff)/1e3
+plt.plot(ground_range/1e3 - grdiff, incidence)
+plt.plot(ground_range[idx]/1e3 - grdiff, incidence[idx], 'g.')
+plt.plot(ground_range[targetIDX]/1e3 - grdiff, incidence[targetIDX], 'ro')
+plt.xlabel("ground range (km)")
+plt.ylabel("Incidence angle (deg)")
+plt.grid()
+plt.title('Ground point: lat: %0.2f (deg), lon %0.2f (deg), hae: %0.2f (m)' % groundPoint)
+plt.show()
 
 #%% Compute time between cycles
-cycle_durations = [np.timedelta64(350729,'m'),
-                   np.timedelta64(350729,'m'),
-                   np.timedelta64(350729,'m'),
-                   np.timedelta64(350487,'m'),
-                   np.timedelta64(350487,'m')
-                  ]
+cycle_durations = np.diff([tascarr[0]] + [tascarr[k] 
+                        for k in cycle_jump])
+# cycle_durations = [np.timedelta64(350729,'m'),
+#                    np.timedelta64(350729,'m'),
+#                    np.timedelta64(350729,'m'),
+#                    np.timedelta64(350487,'m'),
+#                    np.timedelta64(350487,'m')
+#                   ]
 
 #%% Function to find the orbit number
 def getOrbitNumber(timeArray, times):
@@ -113,6 +174,7 @@ def getOrbitNumber(timeArray, times):
 
 #%% Main work cell. Compute incidence angles across cycles
 time_points = sv.measurementTime[svindeces[0]] + np.cumsum(cycle_durations)
+orbitRange = np.arange(-10,11)
 point = {"target": {"xyz": xG.tolist(), 
                     "llh": list(sv.xyz2polar(xG))
                     },
@@ -124,24 +186,26 @@ for eta in tqdm(time_points):
         greater than 60 degrees (i.e. wrong solution), then flip over half 
         an orbit """
     rvec, inc, satSV  = computeImagingGeometry(sv, eta, xG, xG_snormal)
+    
+    """ Check that we're at the right solution """
     if np.abs(inc) > 60:
         eta += np.timedelta64(int(venSAR.period/2), 's')
         rvec, inc, satSV  = computeImagingGeometry(sv, eta, xG, xG_snormal)
     
-    """ The next two lines are to set the period """
+    """ The next two lines are to reset the period """
     orbitAngle, ascendingNode = venSAR.setFromStateVector(satSV[1])
     period = np.timedelta64(int(venSAR.period*1e6),'us')
     
     """ Generate an array of times to examine """
-    etarange = satSV[0] + np.arange(-10,11)*period
+    etarange = satSV[0] + orbitRange*period
     
     """ Compute the imaging geometry to broadside at these times """
     options = [computeImagingGeometry(sv, eta, xG, xG_snormal) 
                for eta in etarange]
     
     """ Compute the orbit number """
-    orbitNumber = [getOrbitNumber(tascarr, 
-                                  [o[2][0] for o in options])]
+    orbitNumber = getOrbitNumber(tascarr, 
+                                 [o[2][0] for o in options])
     
     """ Write to json/dict """
     point["cycle"].append(
@@ -162,18 +226,60 @@ for eta in tqdm(time_points):
         )
 
 #%%
-groundPoint = tuple(sv.xyz2polar(xG))
 symbols = ['.', 'o', 'x', 'd', 's']
+colors = ['red', 'gold', 'slateblue', 'darkviolet', 'limegreen']
 plt.figure()
-myincidences = [[p["incidence"] for p in cycle] for cycle in point["cycle"]]
+myincidences = [[p["incidence"] 
+                 for p in cycle] 
+                for cycle in point["cycle"]]
+
+myorbitNum = [[p["orbitNumber"]%5 for p in cycle] 
+              for cycle in point["cycle"]]
+
 for data,symbol in zip(myincidences, symbols):
-    plt.plot(data, symbol)
+    plt.plot(orbitRange, data, symbol)
+    
 plt.grid()
 x_axis = np.arange(len(myincidences[0]))
-y_axis = np.ones_like(x_axis)*incidence[8]
-plt.plot(x_axis, y_axis)
+y_axis = np.ones_like(x_axis)*incidence[targetIDX]
+plt.plot(orbitRange, y_axis)
 plt.xlabel('orbit')
 plt.ylabel('Incidence Angle (deg)')
 plt.legend(['cycle 2', 'cycle 3', 'cycle 4', 'cycle 5', 'cycle 6', 'cycle 1'])
 plt.title('Incidence to point lat: %0.2f (deg), lon %0.2f (deg), hae: %0.2f (m)' % groundPoint)
 plt.show()
+
+targetInc = incidence[targetIDX][0]
+#%%
+oNS = np.arange(5)
+pl = [[[x for x,k in zip(myinc, myorb) if k == n] 
+        for n in oNS] for myinc, myorb in zip(myincidences,myorbitNum)]
+
+#%% Compute the orbit number on cycle 1
+cycle1orbitNumber = getOrbitNumber(tascarr, 
+                                   [sv.measurementTime[svindeces[0]]])[0]
+cycle1orbitNumber = cycle1orbitNumber%5 + 1
+
+#%% Plot the incidence differences
+for cycleNum in range(5):
+    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+    for k in range(5):
+        r = np.array(pl[cycleNum][k]) - targetInc
+        th = np.angle(np.exp(k*1j*np.pi/5)*r)
+        ax.plot(th, np.abs(r), 'o')
+    
+    ax.set_rticks([1.5, 5, 10]) 
+    xT = np.arange(0,2*np.pi,np.pi/5)
+    xL = ['orbit 1', 
+          'orbit 2', 
+          'orbit 3', 
+          'orbit 4', 
+          'orbit 5', 
+          'orbit 1', 
+          'orbit 2', 
+          'orbit 3', 
+          'orbit 4', 
+          'orbit 5']
+    plt.xticks(xT, xL)
+    plt.title("Cycle %d" % (cycleNum + 2))
+    plt.show()

radar/orbit/geometry.py
@@ -17,8 +17,10 @@ def getTiming(sv, elev, idx = 0):
     T = svdata[3:]/np.linalg.norm(svdata[3:])
     C = np.cross(N, T)
     C = C/np.linalg.norm(C)
-    T = np.cross(C,N)
-    T = T/np.linalg.norm(T)
+    N = np.cross(T, C)
+    N = N/np.linalg.norm(N)
+    # T = np.cross(C,N)
+    # T = T/np.linalg.norm(T)
     uhats = np.array([np.cos(eang)*N + np.sin(eang)*C for eang in elev])
 
     # Calculate range vectors

radar/orbit/pointing.py
@@ -419,11 +419,6 @@ class simulation:
                                    off_nadir, 
                                    covariances,
                                    N = 100000):
-                                   # R_RPY = np.diag([3e-3, 0.4e-3, 3e-3])**2,
-                                   # R_v = np.diag([0.2, 0.2, 0.2])**2,
-                                   # R_t = 5**2,
-                                   # R_p = 430**2,
-                                   # N = 100000):
         """
         This function combines source errors into an aeu covariance 
         matrix. 
@@ -478,7 +473,9 @@ class simulation:
                                           off_nadir, 
                                           np.array(covariances["orbitAlongTrack"]["R"]))
         
-        R_pos = covariances["orbitAcrossTrack"]["R"]/r**2
+        lookvec = np.array([np.cos(np.radians(off_nadir)), np.sin(np.radians(off_nadir))])
+        R_xtrack = np.diag(covariances["orbitAcrossTrack"]["R"])
+        R_pos = lookvec.dot(R_xtrack).dot(lookvec)/r**2
         
         R_ins = covariances["instrument"]["R"]
         
@@ -509,7 +506,7 @@ class simulation:
             The off-nadir angle of the desired look-direction. This angle is
             defined as a right-handed rotation around the velocity vector; 
             thus, a positive angle corresponds to left-looking while a negative
-            angle corresponds to right-looking.
+            angle corresponds to right-looking. Degrees units.
         R_RPY : `np.ndarray(3,3)`
             Covariance matrix of roll, pitch, yaw error.
         R_v : `np.ndarray(3,3)`, optional
@@ -660,10 +657,6 @@ class simulation:
                                                    np.linalg.norm(R),
                                                    off_nadir, 
                                                    covariances)
-                                                   # R_RPY = R_RPY,
-                                                   # R_v = R_v,
-                                                   # R_t = R_t,
-                                                   # R_p = R_p)
         
         R_AEU = AEU_m.dot(R).dot(AEU_m.T)
         aeu_e = self.generateGaussian(R_AEU, n_AEU)

radar/orbit/vensar_tests.py
@@ -67,10 +67,14 @@ covariances = {
         "R": (np.diag([4.53e-3, 0.31e-3, 0.31e-3])**2).tolist()
         },
     "instrument": {
-        "description": "Errors in the pointing of the antenna",
-        "referenceVariables": "AzimuthElevationPitch",
+        "description": """Errors in the pointing of the antenna. From JPL
+                          spreadsheet cells 'SAR APE Pointing Budget'!G24:26
+                          Here we have set cells 'SC Pointing Control'!D10:12
+                          to zero so there is no contribution from the orbit
+                          position error. These values include a 20% margin.""",
+        "referenceVariables": "AzimuthElevationTilt",
         "units": "radians",
-        "R": (np.diag([0.41e-3, 0.97e-3, 0.24e-3])**2).tolist()
+        "R": ((np.diag([0.49e-3, 1.40e-3, 0.23e-3])/2)**2).tolist()
         },
     "orbitVelocity": {
         "description": "Errors in the orbit velocity vector",